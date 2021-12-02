c7b75706f7abcd0f44237820c26b35a041a3b149
ydukhno <edukhno@gmail.com>
2021-12-02 15:48:22 +0100

    Add default regex value for fetching number in RegexTools

--- src/main/kotlin/util/RegexTools.kt
+++ src/main/kotlin/util/RegexTools.kt
@@ -5,7 +5,7 @@ fun String.fetchOccurrence(feature: Regex): String = feature.find(this)!!.value
 fun String.fetchOccurrences(feature: Regex): List<String> =
     feature.findAll(this).map { it.groupValues[0] }.toList()
 
-fun String.fetchNumber(regex: Regex): Int {
+fun String.fetchNumber(regex: Regex = ".+".toRegex()): Int {
     return this
         .fetchOccurrence(regex)
         .fetchOccurrence("\\d+".toRegex())
