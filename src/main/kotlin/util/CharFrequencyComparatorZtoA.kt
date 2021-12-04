0aa294d8d75e1968660e99a4ddd0916b92f4ed3d
ydukhno <edukhno@gmail.com>
2021-12-04 16:14:31 +0100

    Add char frequency comparator with reversed alphabetical order

--- src/main/kotlin/util/CharFrequencyComparatorZtoA.kt
+++ src/main/kotlin/util/CharFrequencyComparatorZtoA.kt
@@ -1,24 +1,24 @@
 package util
 
-class CharFrequencyComparator {
+class CharFrequencyComparatorZtoA { // TODO: refactor comparators to avoid code duplication
     companion object : Comparator<Pair<Char, Int>> {
         override fun compare(o1: Pair<Char, Int>, o2: Pair<Char, Int>): Int = when {
             o1.second != o2.second -> o2.second - o1.second
-            else -> o1.first.compareTo(o2.first)
+            else -> o2.first.compareTo(o1.first)
         }
 
         /**
          * Generates a sorted list of pairs (char, number of occurrences in string) most to least
-         * frequent and then alphabetically.
+         * frequent and then reversed alphabetically.
          */
-        fun getCharFrequencyPairsSorted(text: String): List<Pair<Char, Int>> {
+        fun getCharFrequencyPairsSortedZtoA(text: String): List<Pair<Char, Int>> {
             val frequencyMap = mutableMapOf<Char, Int>()
             for (letter in text) {
                 frequencyMap[letter] = frequencyMap.getOrDefault(letter, 0) + 1
             }
             return frequencyMap
                 .map { Pair(it.key, it.value) }
-                .sortedWith(CharFrequencyComparator)
+                .sortedWith(this)
         }
     }
 }
